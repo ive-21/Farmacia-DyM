61c5c7dec5c9cd6e3227fda22a76374590e9619f
Ive Maria <ivepolanco.21@gmail.com>
2023-05-26 20:31:03 -0400

    Actividad IV - MyDbContext

---
+++ FarmaciaDyM/Data/Context/IMyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using FarmaciaDyM.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FarmaciaDyM.Data.Context
+{
+    public interface IMyDbContext
+    {
+        DbSet<Cliente> Clientes { get; set; }
+        DbSet<Producto> Productos { get; set; }
+        DbSet<Proveedor> Proveedores { get; set; }
+        DbSet<Usuario> Usuarios { get; set; }
+        DbSet<VentaDetalle> VentaDetalles { get; set; }
+        DbSet<Venta> Ventas { get; set; }
+
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default);
+    }
+}
--- FarmaciaDyM/Data/Context/MyDbContext.cs
+++ FarmaciaDyM/Data/Context/MyDbContext.cs
@@ -1,6 +1,38 @@
-﻿namespace FarmaciaDyM.Data.Context
+﻿using FarmaciaDyM.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace FarmaciaDyM.Data.Context
 {
-    public class MyDbContext
+    public class MyDbContext : Microsoft.EntityFrameworkCore.DbContext, IMyDbContext
     {
-    }
+        private readonly IConfiguration configuration;
+
+        public MyDbContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Proveedor> Proveedores { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+        public DbSet<VentaDetalle> VentaDetalles { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: configuration.GetConnectionString(name: "MSSQL"));
+
+        }
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+    };
+
 }
+
+
+
+
+
+
